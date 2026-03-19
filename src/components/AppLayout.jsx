f692bb091b50d9191e88d071624b4eba7f972016
Théo Poizat <hello@zatteo.com>
2026-03-19 08:33:54 +0100

    feat: Update app icons

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -3,10 +3,17 @@ import { Outlet } from 'react-router-dom'
 
 import { BarComponent } from 'cozy-bar'
 
+import Visio from '@/components/Icons/Visio'
+import VisioText from '@/components/Icons/VisioText'
+
 const AppLayout = () => {
   return (
     <>
-      <BarComponent searchOptions={{ enabled: false }} />
+      <BarComponent
+        appIcon={Visio}
+        appTextIcon={VisioText}
+        searchOptions={{ enabled: false }}
+      />
       <Outlet />
     </>
   )
